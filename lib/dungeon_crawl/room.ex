0066be016f1b84a5ddfea4933a3c79ebabb3752a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-03 14:40:51 -0300

    add a room that triggers an enemy

--- lib/dungeon_crawl/room.ex
+++ lib/dungeon_crawl/room.ex
@@ -6,14 +6,15 @@ defmodule DungeonCrawl.Room do
   defstruct description: nil, actions: [], trigger: nil
 
   def all, do: [
-#    %Room{
-#      description: "You found a quiet place. Looks safe for a little nap.",
-#      actions: [forward(), rest()],
-#    },
     %Room{
       description: "You can see the light of day. You found the exit!",
       actions: [forward()],
       trigger: Triggers.Exit
     },
+    %Room{
+      description: "You can see an enemy blocking your path.",
+      actions: [forward()],
+      trigger: Triggers.Enemy
+    }
   ]
 end
