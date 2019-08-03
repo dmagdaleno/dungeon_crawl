be55c99b673b44f1ebcf8327276a4f775e8b1bf9
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-03 15:20:17 -0300

    add the rest room

--- lib/dungeon_crawl/room.ex
+++ lib/dungeon_crawl/room.ex
@@ -6,15 +6,20 @@ defmodule DungeonCrawl.Room do
   defstruct description: nil, actions: [], trigger: nil
 
   def all, do: [
+    #%Room{
+    #  description: "You can see the light of day. You found the exit!",
+    #  actions: [forward()],
+    #  trigger: Triggers.Exit
+    #},
     %Room{
-      description: "You can see the light of day. You found the exit!",
-      actions: [forward()],
-      trigger: Triggers.Exit
+      description: "You found a quiet place. Looks safe for a little nap.",
+      actions: [forward(), rest()],
+      trigger: Triggers.Rest
     },
-    %Room{
-      description: "You can see an enemy blocking your path.",
-      actions: [forward()],
-      trigger: Triggers.Enemy
-    }
+    #%Room{
+    #  description: "You can see an enemy blocking your path.",
+    #  actions: [forward()],
+    #  trigger: Triggers.Enemy
+    #}
   ]
 end
