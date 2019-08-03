1f4aeceb087de573ed61b6d4daf1155dbfc8a7b4
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-02 22:38:35 -0300

    use static types in the trigger function

--- lib/dungeon_crawl/character.ex
+++ lib/dungeon_crawl/character.ex
@@ -1,4 +1,14 @@
 defmodule DungeonCrawl.Character do
+
+  @type t :: %DungeonCrawl.Character{
+    name: String.t,
+    description: String.t,
+    hit_points: non_neg_integer,
+    max_hit_points: non_neg_integer,
+    attack_description: String.t,
+    damage_range: Range.t
+  }
+  
   defstruct name: nil,
   description: nil,
   hit_points: 0,
@@ -9,4 +19,5 @@ defmodule DungeonCrawl.Character do
   defimpl String.Chars do
     def to_string(character), do: character.name
   end
+
 end
--- lib/dungeon_crawl/room/trigger.ex
+++ lib/dungeon_crawl/room/trigger.ex
@@ -1,3 +1,3 @@
 defmodule DungeonCrawl.Room.Trigger do
-  @callback run(character :: any, action :: any) :: any
+  @callback run(Character.t, Action.t) :: {Character.t, atom}
 end
