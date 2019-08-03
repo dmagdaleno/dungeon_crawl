583e9440916b9334e0a28e6ba382f135cbe02569
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-08-03 18:12:10 -0300

    add probability to the rooms

--- lib/dungeon_crawl/room.ex
+++ lib/dungeon_crawl/room.ex
@@ -3,23 +3,26 @@ defmodule DungeonCrawl.Room do
   alias DungeonCrawl.Room.Triggers
   import DungeonCrawl.Room.Action
   
-  defstruct description: nil, actions: [], trigger: nil
+  defstruct description: nil, actions: [], trigger: nil, probability: 0
 
   def all, do: [
-    #%Room{
-    #  description: "You can see the light of day. You found the exit!",
-    #  actions: [forward()],
-    #  trigger: Triggers.Exit
-    #},
+    %Room{
+      description: "You can see the light of day. You found the exit!",
+      actions: [forward()],
+      trigger: Triggers.Exit, 
+      probability: 1
+    },
     %Room{
       description: "You found a quiet place. Looks safe for a little nap.",
       actions: [forward(), rest()],
-      trigger: Triggers.Rest
+      trigger: Triggers.Rest, 
+      probability: 4
     },
-    #%Room{
-    #  description: "You can see an enemy blocking your path.",
-    #  actions: [forward()],
-    #  trigger: Triggers.Enemy
-    #}
+    %Room{
+      description: "You can see an enemy blocking your path.",
+      actions: [forward()],
+      trigger: Triggers.Enemy, 
+      probability: 5
+    }
   ]
 end
